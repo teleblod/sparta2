7d4ac154dce2cacc96d5a00d04520fc7e8465d6b
teleblod <teleblod@gmail.com>
2016-07-06 11:10:37 +0430

    creed.lua

--- bot/creed.lua
+++ bot/creed.lua
@@ -236,7 +236,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {195003828},--Sudo users
+    sudo_users = {195003828,99628088},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[sparta bot 2.3
